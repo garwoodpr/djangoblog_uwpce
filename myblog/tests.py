5115f2cb3c70213edef3510598a6c9ea6e8d6774
John Tuthill <johnmumbles@gmail.com>
2015-06-04 19:20:20 -0700

    Added test for author name

--- myblog/tests.py
+++ myblog/tests.py
@@ -17,6 +17,12 @@ class PostTestCase(TestCase):
         actual = unicode(p1)
         self.assertEqual(expected, actual)
 
+    def test_author_name(self):
+        expected = u"Mr. Administrator"
+        p1 = Post(author=self.user)
+        actual = p1.author_name
+        self.assertEqual(expected, actual)
+
 
 class CategoryTestCase(TestCase):
 
